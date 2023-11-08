338ab990d98930590e85f96df3a336290366a44a
lionlev21 <lionslev21@gmail.com>
2023-11-08 15:00:51 +0200

    Just one more push

--- script.js
+++ script.js
@@ -6,12 +6,15 @@ const ce = document.querySelector("#clear");
 const input = document.querySelector(".input");
 const history = document.querySelector(".history");
 const equals = document.querySelector("#equals");
+const display = document.querySelector(".display");
 let firstNum = 0;
-let secondNum = 0;
+let secondNum;
 let operator = "";
 let operators = Array.from(operands);
+let lastOperator = "";
 let result = 0;
-console.log(operators);
+console.log(display.clientWidth);
+console.log(input.clientWidth);
 function operate(a, b, operand) {
   switch (operand) {
     case "+": {
@@ -23,7 +26,7 @@ function operate(a, b, operand) {
     case "X": {
       return multiply(a, b);
     }
-    case "/": {
+    case "÷": {
       return divide(a, b);
     }
   }
@@ -40,9 +43,11 @@ function multiply(a, b) {
 function divide(a, b) {
   return a / b;
 }
+
 c.addEventListener("click", () => {
   if (input.textContent.length === 1) {
-    input.textContent = "0";
+    input.textContent = 0;
+    history.textContent = 0;
   } else {
     input.textContent = input.textContent.slice(
       0,
@@ -51,7 +56,7 @@ c.addEventListener("click", () => {
   }
 });
 ce.addEventListener("click", () => {
-  input.textContent = "0";
+  input.textContent = 0;
   history.textContent = " ";
   firstNum = 0;
   secondNum = 0;
@@ -64,40 +69,40 @@ numbers.forEach((number) => {
     } else {
       input.textContent = input.textContent.concat(number.textContent);
     }
-  });
-});
-dot.addEventListener(
-  "click",
-  () => (input.textContent = input.textContent + dot.textContent)
-);
-operands.forEach((operand) => {
-  operand.addEventListener("click", () => {
-    firstNum = Number(input.textContent);
+
     operators.forEach((op) => {
       op.style.backgroundColor = "white";
     });
-    // Set the clicked operand to green
+  });
+});
+
+dot.addEventListener("click", () => {
+  if (input.textContent.includes(".")) {
+    alert("You cant put more than one point at a number");
+  } else {
+    input.textContent = input.textContent + dot.textContent;
+  }
+});
+operands.forEach((operand) => {
+  operand.addEventListener("click", () => {
+    firstNum = input.textContent;
+    history.textContent = input.textContent.concat(" ", operand.textContent);
+    input.textContent = 0;
+    lastOperator = operand;
+    operators.forEach((op) => {
+      op.style.backgroundColor = "white";
+    });
     operand.style.backgroundColor = "#38b43e";
     operator = operand.textContent;
-    numbers.forEach((number) => {
-      number.addEventListener("click", () => {
-        if (input.textContent === "0") {
-          input.textContent = number.textContent;
-        } else {
-          input.textContent = input.textContent.concat(number.textContent);
-        }
-      });
-    });
   });
 });
 equals.addEventListener("click", () => {
-  operators.forEach((op) => {
-    op.style.backgroundColor = "white";
-  });
-  secondNum = Number(input.textContent);
+  secondNum = input.textContent;
+  firstNum = Number(firstNum);
+  secondNum = Number(secondNum);
   result = operate(firstNum, secondNum, operator);
+  input.textContent = result;
+  history.textContent = "";
   firstNum = result;
   secondNum = 0;
-  operator = "";
-  input.textContent = String(firstNum);
 });
